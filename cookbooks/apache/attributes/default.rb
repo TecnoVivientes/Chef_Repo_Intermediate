7e2cfbc309ebd1a899972eb6ff3bc202f52895d3
TecnoVivientes <tecnovivientes@gmail.com>
2019-06-27 18:37:19 +0200

    Nuevos atributos,cambios de dokken a vagrant,en el kitchen tenemos dos plataformas, ubuntu y centos,y tests unitarios y de intergración pasados

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,3 +1,9 @@
-default['apache']['package_name'] = 'httpd'
-default['apache']['service_name'] = 'httpd'
-default['apache']['default_index_html'] = '/var/www/html/index.html'
+case node['platform']
+when 'ubuntu'
+    default['apache']['package_name'] = 'apache2'
+    default['apache']['service_name'] = 'apache2'
+else
+    default['apache']['package_name'] = 'httpd'
+    default['apache']['service_name'] = 'httpd'
+end
+    default['apache']['default_index_html'] = '/var/www/html/index.html'
